f2c0f931cb8bffca26a0ed0e831b9573761009bf
ghazwulshaf <ghazwul.s@gmail.com>
2024-01-12 09:41:25 +0800

    feat: activate authenticating for admin dashboard

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -3,7 +3,9 @@
 namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
+use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class LoginController extends Controller
 {
@@ -13,5 +15,34 @@ class LoginController extends Controller
     public function index()
     {
         return view('auth.login');
+    }
+
+    public function authenticate(Request $request): RedirectResponse
+    {
+        $credentials = $request->validate([
+            'username' => ['required'],
+            'password' => ['required'],
+        ]);
+
+        if (Auth::attempt($credentials)) {
+            $request->session()->regenerate();
+
+            return redirect()->intended('admin');
+        }
+
+        return back()->withErrors([
+            'username' => 'The provided credentials do not match our records.',
+        ])->onlyInput('username');
+    }
+
+    public function logout(Request $request): RedirectResponse
+    {
+        Auth::logout();
+
+        $request->session()->invalidate();
+
+        $request->session()->regenerateToken();
+
+        return back();
     }
 }
--- resources/views/dashboard/_layouts/topbar.blade.php
+++ resources/views/dashboard/_layouts/topbar.blade.php
@@ -2,8 +2,11 @@
     <div>
         <span class="block text-xl font-semibold">Dashboard</span>
     </div>
-    <div class="flex items-center gap-2 *:py-1 *:border *:rounded">
-        <span class="block px-2">Admin</span>
-        <button class="px-4 bg-red-500 text-white border-red-700 hover:shadow shadow-gray-400/40">Keluar</button>
+    <div class="flex items-center gap-2">
+        <span class="block px-2 py-1 border rounded">Admin</span>
+        <form action="{{ route('auth.logout') }}" method="POST">
+            @csrf
+            <button type="submit" class="px-4 py-1 bg-red-500 text-white border border-red-700 rounded hover:shadow shadow-gray-400/40">Keluar</button>
+        </form>
     </div>
 </section>
