bb2e26b20712e3d4c30b7c6a3bce328e2549950a
ghazwulshaf <ghazwul.s@gmail.com>
2024-01-12 09:41:43 +0800

    feat: add admin profile page

---
+++ resources/views/dashboard/profile/index.blade.php
@@ -0,0 +1,12 @@
+@extends('dashboard.app')
+
+@section('content')
+<section>
+    <div>
+        <div class="flex items-center gap-2">
+            <span class="text-nowrap">Tag Line</span>
+            <div class="dash-line"></div>
+        </div>
+    </div>
+</section>
+@endsection
